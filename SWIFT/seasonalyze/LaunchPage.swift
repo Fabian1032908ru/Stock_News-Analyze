c06f9b0bf541dff6842cb95b95e9f44de9e1a0f6
Fabian1032908ru <94704751+Fabian1032908ru@users.noreply.github.com>
2023-02-27 10:04:19 +0100

    Add register and login button interface

--- SWIFT/seasonalyze/LaunchPage.swift
+++ SWIFT/seasonalyze/LaunchPage.swift
@@ -35,8 +35,34 @@ class LaunchPage: UIViewController {
         imageview_logo.center = self.view.center
         self.view.addSubview(imageview_logo)
         
+        // Create the first button
+       let button1 = UIButton(type: .system)
+       button1.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+       button1.center = view.center
+       button1.setTitle("Register", for: .normal)
+       button1.setTitleColor(.white, for: .normal)
+       button1.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0)
+       button1.layer.cornerRadius = 10
+       view.addSubview(button1)
+       
+       // Create the second button
+       let button2 = UIButton(type: .system)
+       button2.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+       button2.center = CGPoint(x: view.center.x, y: view.center.y + 70)
+       button2.setTitle("Login", for: .normal)
+       button2.setTitleColor(UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0), for: .normal)
+       button2.backgroundColor = .white
+       button2.layer.cornerRadius = 10
+       button2.layer.borderWidth = 1
+       button2.layer.borderColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0).cgColor
+       view.addSubview(button2)
+        
+        // Alpha 0 to animate buttons
+        button1.alpha = 0
+        button2.alpha = 0
+        
         // Delay animation around 4 seconds and then show login screen
-        let seconds = 1.0
+        let seconds = 0.0
         DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
             
             UIImageView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.55, initialSpringVelocity: 0.5, options: .curveLinear, animations: { [self] in
@@ -46,7 +72,16 @@ class LaunchPage: UIViewController {
                 
             })
             
+            UIButton.animate(withDuration: 0.5, delay: 0.7, animations: {
+                
+                button1.alpha = 1
+                button2.alpha = 1
+                
+            })
+            
         }
+        
+        
         
     }
 
